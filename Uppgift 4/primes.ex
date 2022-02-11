f00f8b1e76362e6e3967348133a4255bbce4611d
Hampus Nilsson <38526557+Hannils@users.noreply.github.com>
2022-02-11 15:51:03 +0100

    Primes

--- Uppgift 4/primes.ex
+++ Uppgift 4/primes.ex
@@ -1,82 +1,94 @@
-defmodule First do
-    def primeList(p) do
-        removePrime(Enum.to_list(2..p))
+  defmodule Bench do
+    def bench(n) do
+      IO.inspect (:timer.tc(fn -> First.first_fun(n) end))
+      IO.inspect (:timer.tc(fn -> Second.second_fun(n) end))
+      IO.inspect (:timer.tc(fn -> Third.third_fun(n) end))
+      :ok
     end
-    def removePrime([h|t]) do
-        removePrime([h|t], h)
+  end
+
+defmodule First do
+
+
+    def first_fun([], primtal) do primtal end
+    def first_fun([h|t], primtal) do
+        acc = List.foldr(t, [], fn x, acc ->
+                cond do
+                    rem(x, h) == 0 -> acc
+                    rem(x, h) != 0 -> [x] ++ acc
+                end
+            end)
+        [h|first_fun(acc, primtal)]
+    end
+    def first_fun(p) do
+      first_fun(Enum.to_list(2..p), [])
     end
 
-    def removePrime([], first) do [] end
-    def removePrime([h|t], first) do
-        cond do
-            rem(h, first) == 0 -> removePrime(t, first)
-            rem(h, first) != 0 -> [h|removePrime(t, first)]
-        end
-    end
+
 end
 
 defmodule Second do
-    def primeList(p) do remover(Enum.to_list(2..p), new()) end
-    def remover(list, primtal) do remover(list, Enum.at(list, 0), 0, primtal) end
-    def remover(list, divValue, acc, primtal) do
-        [h|t] = list
-        valueInList = Enum.at(list, acc)
-        cond do
-            valueInList == nil -> primtal
-            rem(valueInList, divValue) == 0 -> remover(list, divValue, acc+1, primtal)
-            rem(valueInList, divValue) != 0 ->
-                case checkPrimtal(primtal, valueInList, 0) do
-                    0 -> remover(list, divValue, acc+1, primtal)
-                    1 -> 
-                        primtal = add(primtal, valueInList)
-                        remover(list, divValue, acc+1, primtal)
-                end
-        end
+
+
+    def second_fun(n) do
+    list = Enum.to_list(2..n)
+    second_fun(list, [])
     end
 
-    def new() do [] end
-    def add(primtal, s) do
-        primtal = primtal ++ [s]
+    def second_fun(list, primes) do
+      case list do
+        [] -> primes
+        [h|t] ->
+            var = checkP(list, primes)
+            primes = insertP(var, h, primes)
+            second_fun(t, primes)
+      end
     end
-    def checkPrimtal(primtal, s, tracker) do
-        valueAtTracker = Enum.at(primtal, tracker)
-        cond do 
-            valueAtTracker == nil -> 1
-            rem(s, valueAtTracker) == 0 -> 0
-            rem(s, valueAtTracker) != 0 -> checkPrimtal(primtal, s, tracker+1)
-        end
+
+    def checkP(_, []) do true end
+    def checkP([h|t], primes=[h2|t2]) do
+      cond do
+        rem(h, h2) == 0 -> false
+        true -> checkP([h|t], t2)
+      end
+    end
+
+    def insertP(var, x, primes) do
+      case var do
+        true -> primes ++ [x]
+        false -> primes
+      end
     end
 end
 
-defmodule Third do 
-    def primeList(p) do remover(Enum.to_list(2..p), new()) end
-    def remover(list, primtal) do remover(list, Enum.at(list, 0), 0, primtal) end
-    def remover(list, divValue, acc, primtal) do
-        [h|t] = list
-        valueInList = Enum.at(list, acc)
-        cond do
-            valueInList == nil -> Enum.reverse(primtal)
-            rem(valueInList, divValue) == 0 -> remover(list, divValue, acc+1, primtal)
-            rem(valueInList, divValue) != 0 ->
-                case checkPrimtal(primtal, valueInList, 0) do
-                    0 -> remover(list, divValue, acc+1, primtal)
-                    1 -> 
-                        primtal = add(primtal, valueInList)
-                        remover(list, divValue, acc+1, primtal)
-                end
-        end
+defmodule Third do
+    def third_fun(n) do
+    list = Enum.to_list(2..n)
+    third_fun(list, [])
     end
 
-    def new() do [] end
-    def add(primtal, s) do
-        [s|primtal]
+    def insertPrime(bool, x, primes) do
+      case bool do
+        true -> [x] ++ primes
+        false -> primes
+      end
     end
-    def checkPrimtal(primtal, s, tracker) do
-        valueAtTracker = Enum.at(primtal, tracker)
-        cond do 
-            valueAtTracker == nil -> 1
-            rem(s, valueAtTracker) == 0 -> 0
-            rem(s, valueAtTracker) != 0 -> checkPrimtal(primtal, s, tracker+1)
-        end
+
+    def checkPrime(_, []) do true end
+    def checkPrime([h|t], primes=[h2|t2]) do
+      cond do
+        rem(h, h2) == 0 -> false
+        true -> checkPrime([h|t], t2)
+      end
+    end
+
+    def third_fun(list, primes) do
+      case list do
+        [] -> Enum.reverse(primes)
+        [h|t] ->
+            bool = checkPrime(list, primes)
+            primes = insertPrime(bool, h, primes)
+            third_fun(t, primes)
+      end
     end
 end
